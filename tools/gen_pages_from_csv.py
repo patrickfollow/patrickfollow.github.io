0c335b4438e8abf623221e1cd1a681e15a0cf062
Patrick <patrickfollow@gmail.com>
2021-11-11 17:37:48 +0800

    add more site

--- tools/gen_pages_from_csv.py
+++ tools/gen_pages_from_csv.py
@@ -7,11 +7,12 @@
 # @Description: gg
 
 import os
+from urllib.parse import urlparse
 from collections import OrderedDict
 import pandas as pd
 
 default_group = '未分类'
-default_description = '暂无描述'
+default_description = ''
 default_fontawesome = 'fas fa-tools'
 
 data_dir = '../source/_data/'
@@ -19,16 +20,33 @@ config_template = '_config.webstack.template.yml'
 config_target = '../_config.webstack.yml'
 
 
+def parse_other_format(df):
+    items = df.columns.values
+    pages = []
+    for i in range(0, len(items), 3):
+        group = items[i]
+        for j in range(len(df[group])):
+            name = df[items[i]][j]
+            if pd.isna(name): continue
+            url = df[items[i + 1]][j]
+            pages.append({'group': group, 'name': name, 'description': default_description,
+                          'url': url if not pd.isna(url) else '', 'icon': ''})
+    return pages
+
+
 def load_settings(fn):
     pages_df = pd.read_excel(fn, sheet_name=0)
     group_df = pd.read_excel(fn, sheet_name=1)
+    other_df = pd.read_excel(fn, sheet_name=2)
+
+    pages_df = pages_df.append(parse_other_format(other_df), ignore_index=True)
     return pages_df, group_df
 
 
 def get_icon(url):
     if not url.startswith('http'): url = 'http://' + url
-    if url.endswith('/'): return '{}favicon.ico'.format(url)
-    return '{}/favicon.ico'.format(url)
+    parse_result = urlparse(url)
+    return '{}://{}/favicon.ico'.format(parse_result[0], parse_result[1])
 
 
 def get_page_settings(pages_df, group_df):
@@ -38,7 +56,7 @@ def get_page_settings(pages_df, group_df):
             row['description'] = default_description
         if pd.isna(row['group']):
             row['group'] = default_group
-        if pd.isna(row['icon']):
+        if pd.isna(row['icon']) or len(row['icon']) == 0:
             row['icon'] = get_icon(row['url'])
 
         if not row['group'] in pages: pages[row['group']] = []
